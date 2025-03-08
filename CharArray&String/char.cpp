0f0ccffca0a5779f0bc238d63873c1bddead1f64
HarshChaturvedi19 <harshchaturvedi580@gmail.com>
2025-03-08 12:38:17 +0530

    Chararray

---
+++ CharArray&String/char.cpp
@@ -0,0 +1,17 @@
+# include<iostream>
+using namespace std;
+int main()
+{
+    // creatiion of char array 
+    char arr[10];
+// taking input of char Haarray 
+cout<<"Enter the char array"<<endl;
+cin>>arr;
+
+// printing the char array
+cout<<"Printing the char array"<<endl;
+cout<<arr<<endl;
+
+
+    return 0;
+}
