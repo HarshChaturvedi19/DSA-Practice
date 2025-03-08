c448f7c32aa472c74eeea94353f2ef1ac41cc72c
HarshChaturvedi19 <harshchaturvedi580@gmail.com>
2025-03-08 13:12:57 +0530

    toUpper

--- CharArray&String/reverse.cpp
+++ CharArray&String/reverse.cpp
@@ -1,32 +1,68 @@
-// reverse the string 
+// // reverse the string 
+// #include<iostream>
+// #include<cstring>
+// using namespace std;
+
+// void reverseString(char ch[], int n )
+// {
+//     int i =0;
+//     int j = n-1;
+//     while(i<j)
+//     {
+//         swap(ch[i], ch[j]);
+//         i++;
+//         j--;
+//     }
+
+// }
+
+// int main()
+// {
+ 
+//     char ch[100];
+// cout<<"Enter the string: "<<endl;
+//     cin.getline(ch,100);
+
+//     cout<<"Before reversing the string: "<<ch<<endl;
+
+//     reverseString(ch, strlen(ch));
+//     cout<<"After reversing the string: "<<ch<<endl;
+
+//     return 0;
+// }
+
+
+
+
+
+// Convert the all character in the upper case
 #include<iostream>
 #include<cstring>
 using namespace std;
 
-void reverseString(char ch[], int n )
+void convertToUpperCase(char ch[], int n)
 {
-    int i =0;
-    int j = n-1;
-    while(i<j)
+    int index = 0;
+    while(ch[index] != '\0')
     {
-        swap(ch[i], ch[j]);
-        i++;
-        j--;
+        if(ch[index] >= 'a' && ch[index] <= 'z')
+        {
+            ch[index] = ch[index] - 'a' + 'A';
+        }
+        index++;
     }
-
 }
 
 int main()
 {
- 
     char ch[100];
-cout<<"Enter the string: "<<endl;
+    cout<<"Enter the string: "<<endl;
     cin.getline(ch,100);
 
-    cout<<"Before reversing the string: "<<ch<<endl;
+    cout<<"Before converting the string to upper case: "<<ch<<endl;
 
-    reverseString(ch, strlen(ch));
-    cout<<"After reversing the string: "<<ch<<endl;
+    convertToUpperCase(ch, strlen(ch));
+    cout<<"After converting the string to upper case: "<<ch<<endl;
 
     return 0;
 }
