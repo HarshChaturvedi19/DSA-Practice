a49538d74b4bf4f6d12669f2c32f2fc7617f5005
HarshChaturvedi19 <harshchaturvedi580@gmail.com>
2025-03-08 12:51:22 +0530

    Updated code

--- CharArray&String/char.cpp
+++ CharArray&String/char.cpp
@@ -3,15 +3,27 @@ using namespace std;
 int main()
 {
     // creatiion of char array 
-    char arr[10];
+    char arr[20] = {0};
 // taking input of char Haarray 
-cout<<"Enter the char array"<<endl;
-cin>>arr;
+// cout<<"Enter the char array"<<endl;
+// cin>>arr;
 
 // printing the char array
+// get line -> 
+cin.getline(arr, 20);
 cout<<"Printing the char array"<<endl;
 cout<<arr<<endl;
 
 
+// printing each caracter of char array
+// for ( int i =0; i<10; i++)
+// {
+//     cout<<"At indes ;-> "<<i<<" "<<arr[i]<<endl;
+// }
+
+
     return 0;
 }
+
+// this program does not print if there is space in the given input
+// to print the space we have to use getline() function
