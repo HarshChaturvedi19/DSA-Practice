d1cc0db4fb9762a18c118dc41237f17e5f671ec1
HarshChaturvedi19 <harshchaturvedi580@gmail.com>
2025-03-09 06:17:25 +0530

    updated code

--- CharArray&String/removeadj-duplicate.cpp
+++ CharArray&String/removeadj-duplicate.cpp
@@ -1,22 +1,32 @@
-// problem no. -> 1047 ( leetcode)
+#include <iostream>
+using namespace std;
+
 class Solution {
     public:
         string removeDuplicates(string s) {
             string ans = "";
             int index = 0;
-            while(index<s.length())
-            {
-            if(ans.length()>0 && ans[ans.length()-1] == s[index])
-            {
-                ans.pop_back();
+            while (index < s.length()) {
+                if (ans.length() > 0 && ans[ans.length() - 1] == s[index]) {
+                    ans.pop_back();
+                } else {
+                    ans.push_back(s[index]);
+                }
+                index++;
             }
-            else
-            {
-                ans.push_back(s[index]);
-            }
-            index++;
-            
+            return ans;
         }
-        return ans;
-        }
-    };
+};
+
+int main() {
+    Solution sol;
+    string input;
+    
+    cout << "Enter a string: ";
+    cin >> input;
+    
+    string result = sol.removeDuplicates(input);
+    cout << "Output after removing duplicates: " << result << endl;
+    
+    return 0;
+}
