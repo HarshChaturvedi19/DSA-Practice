433d13e39398588d0f4cf5694f39f91343271b70
HarshChaturvedi19 <harshchaturvedi580@gmail.com>
2025-03-09 06:12:56 +0530

    adj-duplicate

---
+++ CharArray&String/removeadj-duplicate.cpp
@@ -0,0 +1,22 @@
+// problem no. -> 1047 ( leetcode)
+class Solution {
+    public:
+        string removeDuplicates(string s) {
+            string ans = "";
+            int index = 0;
+            while(index<s.length())
+            {
+            if(ans.length()>0 && ans[ans.length()-1] == s[index])
+            {
+                ans.pop_back();
+            }
+            else
+            {
+                ans.push_back(s[index]);
+            }
+            index++;
+            
+        }
+        return ans;
+        }
+    };
